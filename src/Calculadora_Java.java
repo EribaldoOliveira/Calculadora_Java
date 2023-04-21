107f0e6881a18c1666f8ba407d4762a18d588993
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-04-21 10:40:49 -0300

    Formatação do Resultado final

--- src/Calculadora_Java.java
+++ src/Calculadora_Java.java
@@ -7,7 +7,7 @@ public class Calculadora_Java {
 				int numero1, numero2, resultado;
 				int opcao;
 				
-				opcao = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite umas das opções a seguir: \n 1 - Adicao \n 2 - Subtracao \n 3 - Multiplicacao \n 4 - Divisao\n", "Leia com atencao!", JOptionPane.QUESTION_MESSAGE));
+				opcao = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite uma das opções a seguir: \n 1 - Adicao \n 2 - Subtracao \n 3 - Multiplicacao \n 4 - Divisao\n", "Leia com atencao!", JOptionPane.QUESTION_MESSAGE));
 				
 				numero1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o primeiro numero: ", "Exercicio Java", JOptionPane.QUESTION_MESSAGE));
 				numero2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o segundo numero: ", "Exercicio Java", JOptionPane.QUESTION_MESSAGE));
@@ -20,7 +20,10 @@ public class Calculadora_Java {
 						
 						//1 - Adicao
 						resultado = numero1 + numero2;
-						System.out.printf("%d + %d = %d", numero1, numero2, resultado);
+						System.out.println("\n************************************************");
+						System.out.println("                     RESPOSTA");
+						System.out.println("************************************************\n");
+						System.out.printf("              RESULTADO DE: %d + %d = %d", numero1, numero2, resultado);
 						
 						//Interrompe
 						break;
